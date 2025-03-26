1af7fcf705dd5a9e2f7ac0f485b192e121854c43
Mohamed Magdy <mohamed.magdyabdelhamid@breadfast.com>
2025-03-26 14:33:24 +0200

    commit

--- src/hooks/useIOSJSInjection.jsx
+++ src/hooks/useIOSJSInjection.jsx
@@ -1,15 +1,41 @@
 import { useState, useEffect } from "react";
 
 const useIOSJSInjection = () => {
-  const [dataFromIOSJSInjection, setDataFromIOSJSInjection] = useState(null);
+  // const [dataFromIOSJSInjection, setDataFromIOSJSInjection] = useState(null);
+
+  // useEffect(() => {
+  //   if (window?.iosJSInjectionData) {
+  //     setDataFromIOSJSInjection(window.iosJSInjectionData);
+  //   }
+  // }, []);
+
+  // return dataFromIOSJSInjection;
+
+  //----------------------------
+
+  const [authToken, setAuthToken] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    if (window?.iosJSInjectionData) {
-      setDataFromIOSJSInjection(window.iosJSInjectionData);
+    // Check if window.receiveToken exists and override it to update React state
+    if (window.receiveToken) {
+      // const originalReceiveToken = window.receiveToken;
+      window.receiveToken = (user, token) => {
+        console.log("Received token from iOS in React:", token);
+        setAuthToken(token); // Set React state
+        setUser(user); // Set React state
+        // originalReceiveToken(token); // Call original to keep localStorage behavior
+      };
+
+      // If the token was already set before React mounted, retrieve it from localStorage
+      // const storedToken = localStorage.getItem("authToken");
+      // if (storedToken) {
+      //   setAuthToken(storedToken);
+      // }
     }
   }, []);
 
-  return dataFromIOSJSInjection;
+  return { user, token: authToken };
 };
 
 export default useIOSJSInjection;
